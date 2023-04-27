4eccaa1e7fe2bc744067b36458d20fe7bd38e7aa
Yuri <your@example.com>
2023-04-27 16:38:40 -0500

    Holaa :)

---
+++ src/main/java/com/tecsup/maven/lab/Lab03_C24Y/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.maven.lab.Lab03_C24Y;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
