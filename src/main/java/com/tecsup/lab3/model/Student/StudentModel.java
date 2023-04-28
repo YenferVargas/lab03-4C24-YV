ca517da5e06d7d75af729b9c2ff8adb0233547a4
Estrella-estrell <vargasosccoyenifer0215@gmail.com>
2023-04-27 22:53:22 -0500

    Cambio Realizado

---
+++ src/main/java/com/tecsup/lab3/model/Student/StudentModel.java
@@ -0,0 +1,11 @@
+package com.tecsup.lab3.model.Student;
+
+public class StudentModel {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola mundo");
+		
+
+	}
+}
