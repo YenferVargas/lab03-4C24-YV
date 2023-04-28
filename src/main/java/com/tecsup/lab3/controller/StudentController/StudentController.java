52742750f81d221182ec2011c3d87febec249323
Yenifer <yenifervargas@tecsup.edu.pe>
2023-04-27 22:32:35 -0500

    Cambio1

--- src/main/java/com/tecsup/lab3/controller/StudentController/StudentController.java
+++ src/main/java/com/tecsup/lab3/controller/StudentController/StudentController.java
@@ -1,5 +1,13 @@
 package com.tecsup.lab3.controller.StudentController;
 
 public class StudentController {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola ");
+		String alumno = "Yenifer";
+		System.out.println(alumno);
+		System.out.println("Hola mundo");
+		
 
+	}
 }
